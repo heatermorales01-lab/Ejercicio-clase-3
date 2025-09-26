87a31dfa29b64ea4e211faf609cd150703638bcc
Heather <heatermorales01@gmail.com>
2025-09-26 13:54:14 -0600

    Clase progra

---
+++ Estudiante.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1Clase3
+{
+    public class Estudiante
+    {
+        //atributos
+        private string Nombre; 
+        public int Edad;
+
+        //constructores
+        public Estudiante() 
+        { 
+        }
+
+        public Estudiante(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+
+        //metodos
+        public void Reir()
+        {
+            Console.WriteLine("me estoy riendo");
+        }
+
+        public String cantar(String cancion)
+        {
+            if (cancion.Equals("Macarena"))
+            {
+                return "Baila tu cuerp...";
+            }
+            else 
+            {
+                return "No me se la canción";
+            }
+               
+        } 
+
+        //setters y getters
+        public string getNombre()
+        {
+            return Nombre;
+        }
+
+        public void setNombre(string nombre)
+        {
+            Nombre = nombre;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Ejercicio1Clase3;
+
+Estudiante e1 = new Estudiante("Heather", 24);
+Estudiante e2 = new Estudiante();
+
+
+Console.WriteLine(e1.getNombre());
+Console.WriteLine(e2.getNombre());
+
+e1.Reir();
+e2.Reir();
+
+String rspta = e1.cantar("");
+Console.WriteLine(rspta);
+
+rspta = e2.cantar("Macarena");
+Console.WriteLine(rspta);
+
+Estudiante e3 = new Estudiante();
+e3.setNombre("Andres");
+
+Console.WriteLine(e3.getNombre());
